e48d2ddac079062fc0365d5706761865e9271d36
Phionx <shanjha.usa@gmail.com>
2016-11-21 18:01:46 -0500

    formating

--- chell.c
+++ chell.c
@@ -9,63 +9,68 @@ static void sig_childactive(int sig) {
     return;
 }
 int main() {
-  while(1) {
-    printf("chell: ");
-    char *command = (char *)malloc(sizeof(char)*256);
-    fgets(command, 256, stdin);
-    char *after = command;
-    char *words[128];
-	char *temp[128];
-    int i, j = 0;
-    while (i < 128) {
-      temp[i] = 0;
-	  words[i] = 0;
-      i++;
-    }
+	while(1) {
+		printf("chell: ");
+		char *commandInit = (char *)malloc(sizeof(char)*256);
+		fgets(commandInit, 256, stdin);
+		char *command =  (char *)malloc(sizeof(char)*256);
+		char *after;
+		char *words[128];
+		char *temp[128];
     
-	i = 0;
-    while (after) {
-      //    printf("After: %s, ", after);
-      temp[i] = strsep(&after, " \n\t");
-      //    printf("words[i]: %s\n", words[i]);
-      i++;
-    }
+		int i, j;
+	
+		while(commandInit){
+			command = strsep(&commandInit, ";");
+			after = command;
+			i = 0;
+			j = 0;
+			while (i < 128) {
+				temp[i] = 0;
+				words[i] = 0;
+				i++;
+			}
+			
+			i = 0;
+			while (after) {
+				//    printf("After: %s, ", after);
+				temp[i] = strsep(&after, " \n\t");
+				//    printf("words[i]: %s\n", words[i]);
+				i++;
+			}
 
-	i=0;
+			i=0;
 
-	while(temp[i]){
-		if(strcmp(temp[i], "") != 0){
-			words[j] = temp[i];
-			i++;
-			j++;
-		} else {
-			i++;
-		}
-	}
+			while(temp[i]){
+				if(strcmp(temp[i], "") != 0){
+				words[j] = temp[i];
+				i++;
+				j++;
+				} else {
+					i++;
+				}
+			}
 		
     
-	if(!strcmp("cd", words[0])) {
-	if(words[1] == 0) {
-		chdir("~");
-      }
-      else {
-		chdir(words[1]);
-      }
-    }
-    else if(!strcmp("exit", words[0])) {
-      return 0;
-    }
-
-    else if(!fork()) 
-      return execvp(words[0], words);
-    else {
-      
-      int *i = (int *)malloc(sizeof(int));
-      signal(SIGINT, sig_childactive);
-      //signal(SIGTSTP, sig_childactive);
-      wait(i);
-      signal(SIGINT, SIG_DFL);
-      //signal(SIGTSTP, SIG_DFL);
-    }
-  }
+			if(!strcmp("cd", words[0])) {
+				if(words[1] == 0) {
+					chdir("~");
+				}
+				else {
+					chdir(words[1]);
+				}
+			} else if(!strcmp("exit", words[0])) {
+				return 0;
+			} else if(!fork()) { 
+				return execvp(words[0], words);
+			} else {
+				int *i = (int *)malloc(sizeof(int));
+				signal(SIGINT, sig_childactive);
+				//signal(SIGTSTP, sig_childactive);
+				wait(i);
+				signal(SIGINT, SIG_DFL);
+				//signal(SIGTSTP, SIG_DFL);
+			}
+		}
+	}
 }
