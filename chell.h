ff800fd34415889be06b87ba17170edf14619425
Phionx <shanjha.usa@gmail.com>
2016-11-25 13:56:08 -0500

    testing

---
+++ chell.h
@@ -0,0 +1,7 @@
+static void sig_childactive(int sig);
+void prompt();
+void chell(char **words);
+void chellFd(char *cmd, int infd, int outfd, int errfd);
+int command(char **words, int infd, int outfd, int errfd);
+int main();
+
