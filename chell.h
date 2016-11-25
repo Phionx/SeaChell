f463b57e6a202c4a2dc0dd010d755f7ee70f9b43
Phionx <shanjha.usa@gmail.com>
2016-11-25 14:10:28 -0500

    header

--- chell.h
+++ chell.h
@@ -1,6 +1,6 @@
 static void sig_childactive(int sig);
 void prompt();
-void chell(char **words);
+void chell(char *words);
 void chellFd(char *cmd, int infd, int outfd, int errfd);
 int command(char **words, int infd, int outfd, int errfd);
 int main();
